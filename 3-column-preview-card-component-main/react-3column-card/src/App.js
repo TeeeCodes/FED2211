5c48fdbf84a00adc91d042e53042348ab05ed09d
TeeeCodes <109982438+TeeeCodes@users.noreply.github.com>
2023-02-27 04:26:02 -0500

    React Columns Finished

--- 3-column-preview-card-component-main/react-3column-card/src/App.js
+++ 3-column-preview-card-component-main/react-3column-card/src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
+import React from "react"
+import './App.css'
+import VehicleComponent from './Component/vehicle_component'
 
 function App() {
 
+ 
   return (
+   
     <div className="App">
-      <vehicleComponent 
-        img="../../assets/icon-sedans.svg"
-        name="Sedans"
-        description="Choose a sedan for its affordability and excellent fuel economy. 
+      <VehicleComponent 
+        img="\assets\icon-sedans.svg"
+        sedan="Sedans"
+        sedanDesc="Choose a sedan for its affordability and excellent fuel economy. 
         Ideal for cruising in the city or on your next road trip."
-      />
-      <vehicleComponent 
-        img="../../assets/icon-suvs.svg"
-        name="SUVS"
-        description="Take an SUV for its spacious interior, power, and versatility. 
+
+        suv="SUVS"
+        suvDesc="Take an SUV for its spacious interior, power, and versatility. 
         Perfect for your next family vacation and off-road adventures."
-       />
-      <vehicleComponent 
-        img="../../assets/icon-luxury.svg"
-        name="Luxury"
-        description="Cruise in the best car brands without the bloated prices. 
+
+        luxury="Luxury"
+        luxuryDesc="Cruise in the best car brands without the bloated prices. 
         Enjoy the enhanced comfort of a luxury rental and arrive in style"
       />
+     
     </div>
   );
 }
--- 3-column-preview-card-component-main/react-3column-card/src/Component/vehicle_component.js
+++ 3-column-preview-card-component-main/react-3column-card/src/Component/vehicle_component.js
@@ -1,45 +1,27 @@
 import './vehicle_component.css';
 
-
-function vehicleComponent(props){
-
-    // const descriptions = [
-    //     { name: 'SEDANS',
-    //       sedanDesc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
-          
-    //       button: <button></button>
-    //     },
-  
-    //     { name: 'SUVS',
-    //       suvsDesc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-          
-    //       button: <button></button>
-    //     },
-  
-    //     { name: 'LUXURY',
-    //       luxuryDesc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
-          
-    //       button: <button></button>
-    //     }
-    // ];
+function VehicleComponent(props){
 
     return(
-        <div>
+        <div className="mainContainer">
             <div className="sedanRectangle">
-                <img src={props.img} alt=""/>
-                <h1>{props.name}</h1>
+                <img  className="Logo" src={props.img} alt=""/>
+                <h1 className='sedanH1'>{props.sedan}</h1>
+                <p className='sedanP'>{props.sedanDesc}</p>
                 <button className="sedanBtn">Learn More</button>
             </div>
 
             <div className="suvRectangle">
                 <img src="react-3column-card\src\assets\icon-suvs.svg" alt=""/>
-                <h1>{props.name}</h1>
+                <h1 className='suvH1'>{props.suv}</h1>
+                <p className='suvP'>{props.suvDesc}</p>
                 <button className="suvBtn">Learn More</button>
             </div>
 
             <div className="luxuryRectangle">
                 <img src="react-3column-card\src\assets\icon-luxury.svg" alt=""/>
-                <h1>{props.name}</h1>
+                <h1 className='luxuryH1'>{props.luxury}</h1>
+                <p className='luxuryP'>{props.luxuryDesc}</p>
                 <button className="luxuryBtn">Learn More</button>
             </div>
 
@@ -47,4 +29,4 @@ function vehicleComponent(props){
     );
 }
 
-export default vehicleComponent
+export default VehicleComponent
