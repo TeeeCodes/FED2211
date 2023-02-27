2be78748ef45c13509bed2ba5e7832d019832928
TeeeCodes <109982438+TeeeCodes@users.noreply.github.com>
2023-02-27 04:41:39 -0500

    image changes

--- 3-column-preview-card-component-main/react-3column-card/src/Component/vehicle_component.js
+++ 3-column-preview-card-component-main/react-3column-card/src/Component/vehicle_component.js
@@ -1,25 +1,28 @@
 import './vehicle_component.css';
+import sedan from "../assets/icon-sedans.svg"
+import suvs from "../assets/icon-suvs.svg"
+import luxury from "../assets/icon-luxury.svg"
 
 function VehicleComponent(props){
 
     return(
         <div className="mainContainer">
             <div className="sedanRectangle">
-                <img  className="Logo" src={props.img} alt=""/>
+                <img  className="sedanLogo" src={sedan} alt=""/>
                 <h1 className='sedanH1'>{props.sedan}</h1>
                 <p className='sedanP'>{props.sedanDesc}</p>
                 <button className="sedanBtn">Learn More</button>
             </div>
 
             <div className="suvRectangle">
-                <img src="react-3column-card\src\assets\icon-suvs.svg" alt=""/>
+                <img  className="suvLogo" src={suvs} alt=""/>
                 <h1 className='suvH1'>{props.suv}</h1>
                 <p className='suvP'>{props.suvDesc}</p>
                 <button className="suvBtn">Learn More</button>
             </div>
 
             <div className="luxuryRectangle">
-                <img src="react-3column-card\src\assets\icon-luxury.svg" alt=""/>
+                <img  className="luxuryLogo" src={luxury} alt=""/>
                 <h1 className='luxuryH1'>{props.luxury}</h1>
                 <p className='luxuryP'>{props.luxuryDesc}</p>
                 <button className="luxuryBtn">Learn More</button>
