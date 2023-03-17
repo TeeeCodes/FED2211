9618f4c73e73e5c5464c7263d1d174655870e4a3
TeeeCodes <109982438+TeeeCodes@users.noreply.github.com>
2023-03-17 02:09:19 -0400

    Multiple Routes Built

--- React Website/react-website/src/App.js
+++ React Website/react-website/src/App.js
@@ -1,7 +1,6 @@
-
 import './App.css';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 
 import NavBar from './Components/NavBar/nav';
 import NextButton from './Components/Button/nxtbutton';
@@ -10,17 +9,18 @@ import PersonalInfo from './Components/Personal_Info/Info';
 import ToggleButton from './Components/Monthly/togglebtn';
 import AddOns from './Components/AddOn_Components/addOn';
 
-
 function App() {
-  return(
-    <Routes>
-      <Route path='/' element={<PersonalInfo />}/>
-      <Route path='/' element={<PlanBoxes />}/>
-      <Route path='/' element={<AddOns />}/>
-    </Routes>
-  )
+
+    return (
+        
+
+      <Routes>
+        <Route path='/' element={<div><NavBar/><PersonalInfo/><NextButton/></div>}/>
+        <Route path='/Planning' element={<div><NavBar/><PlanBoxes/></div>}/>
+        <Route path='/Add-Ons' element={<div><NavBar/><AddOns/><ToggleButton/></div>}/>
+      </Routes>
+
+    )
 }
-
-
 
 export default App;
